de06f10f84c96493754f8e970c28222b1849ff7c
hexahedron1 <elongatedsquareorthobicupola@gmail.com>
2026-02-15 09:43:53 +0300

    changed how pixels are hashed

--- powder/Brushes.cs
+++ powder/Brushes.cs
@@ -9,13 +9,13 @@ public interface IBrush {
 public readonly struct NoiseBrush(params int[] colors) : IBrush {
     [Pure]
     public int GetColor(Pixel pixel) {
-        return colors[new Random(pixel.GetHashCode()).Next(0, colors.Length)];
+        return colors[new Random(pixel.MagicNumber).Next(0, colors.Length)];
     }
 }
 
 public readonly struct PulseNoiseBrush(double sweep, params int[] colors) : IBrush {
     public int GetColor(Pixel pixel) {
-        Random rand = new Random(pixel.GetHashCode());
+        Random rand = new Random(pixel.MagicNumber);
         var (r, g, b) = UnpackColor(colors[rand.Next(0, colors.Length)]);
         double sin = Math.Sin(t + rand.Next(0, 628)/100.0)/2*sweep+0.5;
         r = (byte)(r * sin);
--- powder/Pixel.cs
+++ powder/Pixel.cs
@@ -5,18 +5,20 @@ public enum State {
     Liquid,
     Gas
 }
-public struct Pixel(Material material, int x, int y) : IEquatable<Pixel> {
+public class Pixel(Material material, int x, int y) : IEquatable<Pixel> {
     public Material Material { get; } = material;
     public int X { get; set; } = x;
     public int Y { get; set; } = y;
     public State State { get; set; } = State.Solid;
-    public double Grip = 10.0;
+    public double Grip = 0.0;
     public double Energy = 0.0; // J
     public double IncomingEnergy = 0.0; // J
     public double Temperature = 0.0; // °C
+    public double LastAwake = 0.0;
+    public int MagicNumber { get; } = GlobalRandom.Next(int.MaxValue);
     
-    public bool Equals(Pixel other) {
-        return Material.Equals(other.Material) && X == other.X && Y == other.Y;
+    public bool Equals(Pixel? other) {
+        return Material.Equals(other?.Material) && X == other.X && Y == other.Y;
     }
 
     public override bool Equals(object? obj) {
