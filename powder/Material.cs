92f88ab85da72ed817aa17a276ca0e1e87dc3a97
hexahedron1 <elongatedsquareorthobicupola@gmail.com>
2026-02-15 09:44:37 +0300

    stickyness (just a data point for now)

--- powder/Material.cs
+++ powder/Material.cs
@@ -1,6 +1,6 @@
 namespace powder;
 
-public class Material(string name, IBrush brush, double melt = 1713, double boil = 2950, double density = 2.648, double heatCapacity = 0.7) {
+public class Material(string name, IBrush brush, double melt = 1713, double boil = 2950, double density = 2.648, double heatCapacity = 0.7, double stickyness = 0.01) {
     public IBrush Brush { get; } = brush;
     public string Name { get; } = name;
     public List<Reaction> Reactions { get; } = [];
@@ -9,5 +9,6 @@ public class Material(string name, IBrush brush, double melt = 1713, double boil
     public double BoilPoint { get; set; } = boil;
     public double Density { get; set; } = density; // g/cm³; since each pixel is 1cm³ internally, this is effectively mass of a pixel
     public double HeatCapacity { get; set; } = heatCapacity; // J/g·°C
+    public double Stickyness { get; set; } = stickyness; // how much grip it gains for each second passed not moving
 }
 
